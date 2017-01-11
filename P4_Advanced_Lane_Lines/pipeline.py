723c14b989106184e2da91bb959037b5d77f941f
philbort <phduan@gmail.com>
2017-01-10 21:57:40 -0800

    save processed images

--- P4_Advanced_Lane_Lines/pipeline.py
+++ P4_Advanced_Lane_Lines/pipeline.py
@@ -4,18 +4,16 @@ import cv2
 import matplotlib.pyplot as plt
 import glob
 
-# Calibrate camera given calibration images
 def camera_calibration(directory, nx, ny, draw = False):
-
   images = glob.glob(os.path.join(directory, 'calibration*'))
   objpoints = []
   imgpoints = []
   objp = np.zeros((nx*ny, 3), np.float32)
   objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
-  cnt = 0
-  for file in images:
+
+  for fname in images:
     # Read the calibration image
-    img = cv2.imread(file)
+    img = cv2.imread(fname)
     # Converting an image, imported by cv2 or the glob API, to grayscale:
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # Finding chessboard corners
@@ -32,7 +30,7 @@ def camera_calibration(directory, nx, ny, draw = False):
         plt.imshow(img)
         plt.show()
     else:
-      print('Could not find corners from %s' %file)
+      print('Could not find corners from %s' %fname)
 
   # Camera calibration, given object points, image points, and the shape of the grayscale image:
   ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
@@ -40,25 +38,33 @@ def camera_calibration(directory, nx, ny, draw = False):
   # Return the camera matrix and the distortion coefficients
   return mtx, dist
 
-# Use calibrated camera parameters to undistort images
+# Use calibrated camera parameters to undistort an image
 def camera_undistort(img, mtx, dist):
-
   undist = cv2.undistort(img, mtx, dist, None, mtx)
   return undist
 
 if __name__ == '__main__':
 
+  # Calibrate camera with calibration images
   mtx, dist = camera_calibration(directory = 'camera_cal/', nx = 9, ny = 6, draw = False)
 
+  # Directory to save the processed images
+  dest = 'proc_images'
+  if not os.path.exists(dest):
+    os.makedirs(dest)
+
+  # Grab all the test images
   test_imgs = glob.glob(os.path.join('test_images/', 'test*.jpg'))
-  for file in test_imgs:
-    img = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB)
+  for fname in test_imgs:
+    # Get the original image
+    img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
+    # Get the undistorted image
     undist = camera_undistort(img = img, mtx = mtx, dist = dist)
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize = (24, 9))
-    #f.tight_layout()
-    ax1.imshow(img)
-    ax1.set_title('Original Image', fontsize = 50)
-    ax2.imshow(undist)
-    ax2.set_title('Undistorted Image', fontsize = 50)
-    plt.subplots_adjust(left = 0.0, right = 1.0, top = 0.9, bottom = 0.0)
-    plt.show()
+    # Plot the image
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    ax.imshow(undist)
+    # Savefig doesn't work for jpg, use png instead
+    savefig_name = 'undistorted_' + fname.split('/')[1].split('.')[0] + '.png'
+    # Save the image
+    fig.savefig(os.path.join(dest,savefig_name))
