e6f15f9974c779a2e07a0bf85971ef55a58e1500
philbort <phduan@gmail.com>
2017-01-13 21:43:15 -0800

    Add Sobel threshold

--- P4_Advanced_Lane_Lines/pipeline.py
+++ P4_Advanced_Lane_Lines/pipeline.py
@@ -43,6 +43,24 @@ def camera_undistort(img, mtx, dist):
   undist = cv2.undistort(img, mtx, dist, None, mtx)
   return undist
 
+def abs_sobel_thresh(img, orient = 'x', thresh_min = 0, thresh_max = 255):
+  # Convert to grayscale
+  gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+  # Apply x or y gradient with the OpenCV Sobel() function
+  # and take the absolute value
+  if orient == 'x':
+    abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+  elif orient == 'y':
+    abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+  # Rescale back to 8 bit integer
+  scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+  # Create a copy of the image
+  binary_output = np.zeros_like(scaled_sobel)
+  # Apply the threshold
+  binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+  return binary_output
+
 if __name__ == '__main__':
 
   # Calibrate camera with calibration images
@@ -64,7 +82,17 @@ if __name__ == '__main__':
     fig = plt.figure()
     ax = plt.subplot(111)
     ax.imshow(undist)
+    plt.xticks([])
+    plt.yticks([])
     # Savefig doesn't work for jpg, use png instead
     savefig_name = 'undistorted_' + fname.split('/')[1].split('.')[0] + '.png'
     # Save the image
     fig.savefig(os.path.join(dest,savefig_name))
+
+  undistorted_imgs = glob.glob(os.path.join(dest, '*.png'))
+  for fname in undistorted_imgs:
+    bianry_img = abs_sobel_thresh(cv2.imread(fname), 'x', 20, 100)
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    ax.imshow(bianry_img, cmap='gray')
+    plt.show()
