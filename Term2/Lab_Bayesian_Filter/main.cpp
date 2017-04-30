d9db766b09bfc049edd4a40e8b8f637f8ca205e2
philbort <phduan@gmail.com>
2017-04-29 17:27:43 -0700

    add image notes from the lab

--- Term2/Lab_Bayesian_Filter/main.cpp
+++ Term2/Lab_Bayesian_Filter/main.cpp
@@ -13,74 +13,70 @@ using namespace std;
 
 int main()
 {
-	/******************************************************************************
-	 *  declaration:															      
-	 *****************************************************************************/
-	
-	//define example: 01, 02, 03, 04
-	string example_string = "04";
+    /******************************************************************************
+     *  declaration:                                                                  
+     *****************************************************************************/
+    
+    //define example: 01, 02, 03, 04
+    string example_string = "04";
 
-	//declare map:
-	map map_1d;
+    //declare map:
+    map map_1d;
 
-	//declare measurement list:
-	vector<MeasurementPackage> measurement_pack_list;
+    //declare measurement list:
+    vector<MeasurementPackage> measurement_pack_list;
 
-	//declare helpers:
-	help_functions helper;
+    //declare helpers:
+    help_functions helper;
 
-	//define file names:
-	char in_file_name_ctr[1024];
-	char in_file_name_obs[1024];
-	char in_file_name_gt[1024];
+    //define file names:
+    char in_file_name_ctr[1024];
+    char in_file_name_obs[1024];
+    char in_file_name_gt[1024];
 
-	/******************************************************************************
-	 *  read map and measurements:											     *
-	 *****************************************************************************/
-	//read map:
-	helper.read_map_data("data/map_1d.txt", map_1d);
+    /******************************************************************************
+     *  read map and measurements:                                               *
+     *****************************************************************************/
+    //read map:
+    helper.read_map_data("data/map_1d.txt", map_1d);
 
-	//define file name of controls:
-	sprintf(in_file_name_ctr, "data/example%s/control_data.txt", 
-			example_string.c_str());
+    //define file name of controls:
+    sprintf(in_file_name_ctr, "data/example%s/control_data.txt", 
+            example_string.c_str());
 
-	//define file name of observations:
-	sprintf(in_file_name_obs, "data/example%s/observations/", 
-			example_string.c_str());
-	
-	//read in data to measurement package list:
-	helper.read_measurement_data(in_file_name_ctr, 
-			                     in_file_name_obs, 
-			                     measurement_pack_list);
+    //define file name of observations:
+    sprintf(in_file_name_obs, "data/example%s/observations/", 
+            example_string.c_str());
+    
+    //read in data to measurement package list:
+    helper.read_measurement_data(in_file_name_ctr, 
+                                 in_file_name_obs, 
+                                 measurement_pack_list);
 
-	/*******************************************************************************
-	 *  start 1d_bayesian filter												   *
-	 *******************************************************************************/
+    /*******************************************************************************
+     *  start 1d_bayesian filter                                                   *
+     *******************************************************************************/
 
-	//create instance of 1d_bayesian localization filter:
-	bayesianFilter localization_1d_bayesian;
+    //create instance of 1d_bayesian localization filter:
+    bayesianFilter localization_1d_bayesian;
 
-	//define number of time steps:
-	size_t T = measurement_pack_list.size();
+    //cycle:
+    for (size_t t = 0; t < measurement_pack_list.size(); ++t)
+        //Call 1d_bayesian filter:
+        localization_1d_bayesian.process_measurement(measurement_pack_list[t],
+                                                     map_1d,
+                                                     helper);
 
-	//cycle:
-	for (size_t t = 0; t < T; ++t){
+    /*******************************************************************************
+     *  print/compare results:                                                 *
+     ********************************************************************************/
+    
+    //define file name of gt data:
+    sprintf(in_file_name_gt, "data/example%s/gt_example%s.txt",example_string.c_str(),example_string.c_str() );
 
-		//Call 1d_bayesian filter:
-		localization_1d_bayesian.process_measurement(measurement_pack_list[t],
-				                                     map_1d,
-				                                     helper);
-	}
+    ///compare gt data with results:
+    helper.compare_data(in_file_name_gt, localization_1d_bayesian.bel_x);
+    
 
-	/*******************************************************************************
-	 *  print/compare results:												   *
-	 ********************************************************************************/
-	//define file name of gt data:
-	sprintf(in_file_name_gt, "data/example%s/gt_example%s.txt",example_string.c_str(),example_string.c_str() );
-
-	///compare gt data with results:
-	helper.compare_data(in_file_name_gt, localization_1d_bayesian.bel_x);
-	
-
-	return 0;
+    return 0;
 }
