c616e056fe8e188a1a507b9de75cd0bb19ac00af
philbort <phduan@gmail.com>
2017-05-21 22:46:49 -0700

    finished two lab tests

--- Term2/Lab_Model_Predictive_Control/global_kinematic_model/src/main.cpp
+++ Term2/Lab_Model_Predictive_Control/global_kinematic_model/src/main.cpp
@@ -18,12 +18,29 @@ const double Lf = 2;
 // NOTE: state is [x, y, psi, v]
 // NOTE: actuators is [delta, a]
 Eigen::VectorXd globalKinematic(Eigen::VectorXd state,
-                                Eigen::VectorXd actuators, double dt) {
+                                Eigen::VectorXd actuators, 
+                                double dt)
+{
   Eigen::VectorXd next_state(state.size());
+
+  const double x = state(0);
+  const double y = state(1);
+  const double psi = state(2);
+  const double v = state(3);
+
+  const double delta = actuators(0);
+  const double a = actuators(1);
+
+  next_state(0) = x + v * cos(psi) * dt;
+  next_state(1) = y + v * sin(psi) * dt;
+  next_state(2) = psi + v / Lf * delta * dt;
+  next_state(3) = v + a * dt;
+
   return next_state;
 }
 
-int main() {
+int main()
+{
   // [x, y, psi, v]
   Eigen::VectorXd state(4);
   // [delta, v]
